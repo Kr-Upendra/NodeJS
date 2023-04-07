edb24b1a946a0d5939da591f8a502c7ad45da554
Upendra Kumar <toxic74412@gmail.com>
2023-04-07 23:59:07 +0530

    Some changes in code

--- QR Generator/server.js
+++ QR Generator/server.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Listening at http://127.0.0.1:${port}`);
+  console.log(`Listening at http://127.0.0.1:${port}/home`);
 });
