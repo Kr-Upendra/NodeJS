b161345e03e5e38fa58020234557037c6a81b681
Upendra Kumar <toxic74412@gmail.com>
2023-06-04 00:53:45 +0530

    Created database connect in Basic user system

--- Basic User System/server.js
+++ Basic User System/server.js
@@ -1,6 +1,14 @@
 require("dotenv").config();
 const app = require("./app");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 4545;
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB)
+  .then(console.log("DATABASE CONNECTED!"))
+  .catch((err) => console.error("DATABASE COULDN'T CONNECT!"));
 
 app.listen(port, () => {
   console.log(`server started at http://127.0.0.1:${port}`);
