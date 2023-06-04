acbda371de15abc59136ee32b1e9ac598d90deff
Upendra Kumar <toxic74412@gmail.com>
2023-06-04 15:34:29 +0530

    Created signup route and also implemented globalErrorHandling and Errorclass

--- Basic User System/app.js
+++ Basic User System/app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
+
+const userRoute = require("./routes/userRoute");
+const AppError = require("./utils/AppError");
+const globalErrorHandler = require("./controllers/errorController");
+
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/api/users/", userRoute);
+
+app.all("*", (req, res, next) => {
+  next(
+    new AppError(
+      `requested url ${req.originalUrl} doesn't exist on this server!`,
+      404
+    )
+  );
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ Basic User System/utils/AppError.js
@@ -0,0 +1,9 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode || 500;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "Error 🔥";
+  }
+}
+
+module.exports = AppError;
