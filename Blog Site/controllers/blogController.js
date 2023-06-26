48e8800861e016951fcdccd80a1cb4bbe967ed98
Upendra Kumar <toxic74412@gmail.com>
2023-06-26 22:38:36 +0530

    Created CRUD FOR BLOG SITE WITH MYSQL

--- Blog Site/controllers/blogController.js
+++ Blog Site/controllers/blogController.js
@@ -1,34 +1,149 @@
+const Blog = require("../dbConnection/dbConnection");
+
 exports.getAllBlog = (req, res) => {
-  res.status(200).json({
-    status: "SUCCESS",
-    message: "GET ALL BLOGS LIST!",
+  Blog.query("SELECT * FROM blogs", (err, result) => {
+    if (err) {
+      return res.status(404).json({
+        status: "FAILED",
+        error: err,
+      });
+    } else
+      res.status(200).json({
+        status: "SUCCESS",
+        message: "GET ALL BLOGS LIST!",
+        length: result.length,
+        docs: result,
+      });
   });
 };
 
 exports.getBlog = (req, res) => {
-  res.status(200).json({
-    status: "SUCCESS",
-    message: "GET A BLOG!",
+  const ID = req.params.id;
+  Blog.query(`SELECT * FROM blogs WHERE ID = ${ID}`, (err, result) => {
+    if (err) {
+      return res.status(404).json({
+        status: "FAILED",
+        message: "COULDN'T FOUND BLOG WITH GIVEN ID",
+        error: err,
+      });
+    } else {
+      if (result.length < 1)
+        return res.status(404).json({
+          status: "FAILED",
+          message: "COULDN'T FOUND BLOG WITH GIVEN ID",
+        });
+
+      res.status(200).json({
+        status: "SUCCESS",
+        message: `BLOG WITH GIVEN ID: ${ID}!`,
+        docs: result,
+      });
+    }
   });
 };
 
 exports.createBlog = (req, res) => {
-  res.status(201).json({
-    status: "SUCCESS",
-    message: "New Database Created!",
-  });
+  const { title, postImage, description } = req.body;
+  Blog.query(
+    "INSERT INTO blogs (title, postImage, postedBy, description) VALUES (?, ?, ?, ?)",
+    [title, postImage, "Admin", description],
+    (err, result) => {
+      if (err) {
+        return res.status(400).json({
+          status: "FAILED",
+          error: err,
+        });
+      } else
+        res.status(201).json({
+          status: "SUCCESS",
+          message: "NEW BLOG CREATED!",
+          docs: result,
+        });
+    }
+  );
 };
 
 exports.updateBlog = (req, res) => {
-  res.status(200).json({
-    status: "SUCCESS",
-    message: "BLOG UPDATED!",
-  });
+  const ID = req.params.id;
+  const updatedValue = req.body;
+
+  const keys = Object.keys(updatedValue);
+  const values = Object.values(updatedValue);
+
+  const placeHolders = keys.map((key) => `${key} = ?`).join(", ");
+
+  Blog.query(
+    `UPDATE blogs SET ${placeHolders} WHERE ID = ?`,
+    [...values, ID],
+    (err, result) => {
+      if (err) {
+        return res.status(404).json({
+          status: "FAILED",
+          message: "COULDN'T FOUND BLOG WITH GIVEN ID",
+          error: err,
+        });
+      } else {
+        if (result === null) {
+          return res.status(404).json({
+            status: "FAILED",
+            message: "COULDN'T FOUND BLOG WITH GIVEN ID",
+          });
+        } else {
+          res.status(200).json({
+            status: "SUCCESS",
+            message: `BLOG WITH GIVEN ID: ${ID}!`,
+            docs: result,
+          });
+        }
+      }
+    }
+  );
 };
 
 exports.deleteBlog = (req, res) => {
-  res.status(204).json({
-    status: "SUCCESS",
-    message: "BLOG DELETED!",
+  const ID = req.params.id;
+  Blog.query(`DELETE FROM blogs WHERE ID = ${ID}`, (err, result) => {
+    if (err) {
+      return res.status(404).json({
+        status: "FAILED",
+        message: "COULDN'T FOUND BLOG WITH GIVEN ID",
+        error: err,
+      });
+    } else {
+      if (result === null) {
+        return res.status(404).json({
+          status: "FAILED",
+          message: "COULDN'T FOUND BLOG WITH GIVEN ID",
+        });
+      } else {
+        res.status(204).json({
+          status: "SUCCESS",
+        });
+      }
+    }
   });
 };
+
+/*
+Blog.query(`SELECT * FROM blogs WHERE ID = ${ID}`, (err, result) => {
+    if (err) {
+      return res.status(404).json({
+        status: "FAILED",
+        message: "COULDN'T FOUND BLOG WITH GIVEN ID",
+        error: err,
+      });
+    } else {
+      if (result.length < 1)
+        return res.status(404).json({
+          status: "FAILED",
+          message: "COULDN'T FOUND BLOG WITH GIVEN ID",
+        });
+
+      res.status(200).json({
+        status: "SUCCESS",
+        message: `BLOG WITH GIVEN ID: ${ID}!`,
+        docs: result,
+      });
+    }
+  });
+*/
