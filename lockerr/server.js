6b9cb0152484ec9842e04111d57e299185674ac3
Upendra Kumar <toxic74412@gmail.com>
2023-06-26 20:41:09 +0530

    modified server.js file in lockerr application

--- lockerr/server.js
+++ lockerr/server.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
-import app from "./app";
+import app from "./app.js";
+import connectDB from "./db/connectWithDb.js";
 
 const port = process.env.PORT || 3000;
+const db = process.env.DATABASE;
 
-app.listen(port, () => {
-  console.log(`server started at http://127.0.0.1:${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(db);
+    console.log(`server started at http://127.0.0.1:${port}`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
