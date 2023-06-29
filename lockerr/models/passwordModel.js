6585ebcea59a2ffd07518651dd9a159b82d3b463
Upendra Kumar <toxic74412@gmail.com>
2023-06-29 16:43:04 +0530

    Created Model

---
+++ lockerr/models/passwordModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const passwordSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    minlength: 3,
+    maxlength: 25,
+  },
+  email: {
+    type: String,
+  },
+  website: {
+    type: String,
+    required: [true, "Please provide website name!"],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password!"],
+  },
+  description: {
+    type: String,
+    maxlength: 80,
+  },
+});
+
+const Password = mongoose.model("Password", passwordSchema);
+
+module.exports = Password;
